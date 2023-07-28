4844ec4575dd948a8c5d648ff11004630482fe44
boro-dach <monstriqq1@gmail.com>
2023-07-28 19:50:28 +0200

    my virtual pets

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-document.getElementsByClassName('butt')[0].addEventListener('click', log)
+document.getElementsByClassName('butt')[0].addEventListener('click', addPet)
 
-function log(){
-    console.log('clicked')
+let activeButtons = 0
+
+function addPet(){    
+    if(activeButtons === 0){
+        document.getElementsByClassName('pig')[0].style.display = 'block';
+        activeButtons = activeButtons + 1;
+    }
+    else if(activeButtons === 1){
+        document.getElementsByClassName('dog')[0].style.display = 'block';
+        activeButtons = activeButtons + 1;
+    }
+    else if(activeButtons === 2){
+        document.getElementsByClassName('penguin')[0].style.display = 'block';
+        activeButtons = activeButtons + 1;
+    }
+    else if(activeButtons === 3){
+        document.getElementsByClassName('duck')[0].style.display = 'block';
+        activeButtons = activeButtons + 1;
+    }
+    else if(activeButtons === 4){
+        document.getElementsByClassName('error_message')[0].style.display = 'block';
+        setTimeout(() => {
+            document.getElementsByClassName('error_message')[0].style.display = 'none';
+        }, 2000)
+    }
 }
